e2ebbbc3c9bb72db737e9c270c377298e034d6fd
Alex Clemmer <clemmer.alexander@gmail.com>
2019-04-30 16:07:01 -0700

    Add `__pulumiType` to `CustomResource` class
    Running `pulumi query` over state resources will require the ability to
    filter resources by type.
    
    This commit begins the process of making this possible simply, using the
    TypeScript user-defined type guards feature. This commit changes the
    `CustomResource` constructor to record the `t` argument in the
    `__pulumiType` field, which acts as a "sentinel" value in the resource
    base, which deriving classes need only check to ensure they are of that
    type. For example:
    
        aws.s3.Bucket.isInstance(someOb)
    
    would check the `aws.s3.Bucket.__pulumiType` field, and make sure that
    the type field lines up.

--- sdk/nodejs/resource.ts
+++ sdk/nodejs/resource.ts
@@ -277,6 +277,14 @@ export abstract class CustomResource extends Resource {
     public readonly __pulumiCustomResource: boolean;
 
     /**
+     * @internal
+     * Private field containing the type ID for this object. Useful for implementing `isInstance` on
+     * classes that inherit from `CustomResource`.
+     */
+    // tslint:disable-next-line:variable-name
+    public readonly __pulumiType: string;
+
+    /**
      * id is the provider-assigned unique ID for this managed resource.  It is set during
      * deployments and may be missing (undefined) during planning phases.
      */
@@ -310,6 +318,7 @@ export abstract class CustomResource extends Resource {
 
         super(t, name, true, props, opts);
         this.__pulumiCustomResource = true;
+        this.__pulumiType = t;
     }
 }
 
